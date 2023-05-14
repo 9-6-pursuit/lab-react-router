669cb2324df6580a559989a8613678f253725b6f
Phillip Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-05-14 12:26:31 -0400

    DONE THANKS 2 MY GROUP AND A LITTLE CHAT GPT HELP LOL

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import PetsList from "./components/pets/PetsList";
 import { employeeData } from "./data/employees.js";
 import { ownerData } from "./data/owners";
 import { petData } from "./data/pets";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [employees] = useState(employeeData);
@@ -25,11 +26,16 @@ function App() {
 
   return (
     <div className="wrapper">
-      <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
-      <Footer />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home employees={employees} owners={owners} pets={pets} />} />
+          <Route path="/staff" element={<StaffList employees={employees} />} />
+          <Route path="/pets" element={<PetsList pets={pets} />} />
+          <Route path="/pets/:id" element={<PetsList pets={pets} />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
