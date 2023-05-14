a5a627bba53fa01fb605e266c8f19c3a2a1b1b25
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-05-14 14:50:32 -0400

    Finished react router lab

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 /*
   Components
@@ -25,11 +26,35 @@ function App() {
 
   return (
     <div className="wrapper">
-      <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
-      <Footer />
+      <Router>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<Home employees={employees} owners={owners} pets={pets}/>} />
+          <Route 
+            path="/staff" 
+            element={<StaffList employees={employees}/>}
+          />
+          <Route
+          path="/pets"
+          element={<PetsList pets={pets} type={"pets"}/>}
+          />
+          <Route
+          path="/pets/cats"
+          element={<PetsList pets={pets} type={"cat"}/>}
+          />
+          <Route
+          path="/pets/dogs"
+          element={<PetsList pets={pets} type={"dog"}/>}
+          />
+
+        {/* 
+
+        <StaffList employees={employees} />
+        <PetsList pets={pets} />
+         */}
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
