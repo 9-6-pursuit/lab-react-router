83302da7ac275b0094e6943ae90585e4ec1789bf
crsvalle <cristian.valle@pursuit.org>
2023-05-14 12:58:43 -0400

    router stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 /*
   Components
@@ -25,11 +26,17 @@ function App() {
 
   return (
     <div className="wrapper">
-      <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
-      <Footer />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home employees={employees} owners={owners} pets={pets} />}>
+          </Route>
+          <Route path="/staff" element={<StaffList employees={employees} />}></Route>
+          <Route path="/pets" element={<Navigate replace to="/pets/cats" pets={pets} />}> </Route>
+          <Route path="/pets/:id" element={<PetsList pets={pets} />}> </Route>
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
