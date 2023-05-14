82676877312b0469ba1ee04bf837aa4b64e36e14
Judy <judychue@pursuit.org>
2023-05-14 15:07:56 -0400

    Passed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+// import
 import { useState } from "react";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 /*
   Components
 */
@@ -22,13 +23,37 @@ function App() {
   const [employees] = useState(employeeData);
   const [owners] = useState(ownerData);
   const [pets] = useState(petData);
+  const cats = pets.filter((pet) => {
+    return pet.kind === "Cat"
+  });
+  const dogs = pets.filter((pet) => {
+    return pet.kind === "Dog"
+  })
 
   return (
     <div className="wrapper">
-      <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
+        <Nav />
+          <Router>
+            <Routes>
+              <Route path= "/" element = {<Home employees={employees} owners={owners} pets={pets}/>}/>
+              <Route path= "/staff" element = {<StaffList employees={employees} />}/>
+              <Route 
+                  path= "/pets"
+                  element = {<PetsList pets={pets} kind = {'pets/dogs'}/>}
+                  />
+              <Route 
+                  path= "/pets/cats"
+                  element = {<PetsList pets={cats} kind = {'cats'}/>}
+                  />
+              <Route 
+                  path= "/pets/dogs"
+                  element = {<PetsList pets={dogs} kind = {'dogs'}/>}
+                  />
+
+          {/*
+          <PetsList pets={pets} /> */}
+          </Routes>
+          </Router>
       <Footer />
     </div>
   );
--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,6 +1,7 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export const PetsList = ({ pets }) => {
   const [cats, dogs] = pets.reduce(
@@ -17,11 +18,13 @@ export const PetsList = ({ pets }) => {
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
         {/* All cats section */}
+        {/* Route */}
         {cats.map((cat) => (
           <Pet key={cat.id} kind="cat" pet={cat} />
         ))}
 
         {/* All dogs section */}
+        {/* Route */}
         {dogs.map((dog) => (
           <Pet key={dog.id} kind="dog" pet={dog} />
         ))}
