cd6dd009e6a30666830a88973cefd4e6f7730809
Raymond <raymondjohnson@pursuit.org>
2023-05-15 15:04:54 -0400

    sticky footer; initial 404 page setup

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from "./components/common/Footer";
 import Home from "./components/home/Home";
 import StaffList from "./components/staff/StaffList";
 import PetsList from "./components/pets/PetsList";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 /*
   Data
@@ -17,19 +18,36 @@ import PetsList from "./components/pets/PetsList";
 import { employeeData } from "./data/employees.js";
 import { ownerData } from "./data/owners";
 import { petData } from "./data/pets";
+import PageNotFound from "./components/common/PageNotFound";
 
 function App() {
   const [employees] = useState(employeeData);
   const [owners] = useState(ownerData);
   const [pets] = useState(petData);
+  const cats = pets.filter((pet) => {
+    return pet.kind==="Cat"})
+  const dogs = pets.filter((pet) => {
+    return pet.kind==="Dog"})
 
   return (
     <div className="wrapper">
+      <Router>
       <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
-      <Footer />
+        <Routes>
+        <Route path= "/" element = {<Home employees={employees} owners={owners} pets={pets} />}/>
+        <Route path= "/staff" element = {<StaffList employees={employees} />}/>
+        <Route path= "/pets" 
+          element = {<PetsList pets={pets} type={"pets"}/>}/>
+        <Route path= "/pets/cats" 
+          element = {<PetsList pets={cats} type={"cats"}/>}/>
+        <Route path= "/pets/dogs" 
+          element = {<PetsList pets={dogs} type={"dogs"}/>}/>
+       
+          {/* <PageNotFound employees={employees} />
+          <PetsList pets={pets} /> */}
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/common/PageNotFound.js
@@ -0,0 +1,12 @@
+import "./PageNotFound.css";
+
+function PageNotFound() {
+  return (
+    <div>
+      <h3>This is the 404 error page!</h3>
+      <img src= "../assets/error-page.png"> alt="Error Page" ></img>
+    </div>
+  );
+}
+
+export default PageNotFound;
