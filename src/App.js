fd58d6c28c43204cc0c9013d850bf2638d8d01f7
Berlin Rivas <berlinrivas@Berlins-MacBook-Air.local>
2023-05-14 22:50:48 -0400

    i hate animals now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 /*
   Components
 */
@@ -24,13 +25,45 @@ function App() {
   const [pets] = useState(petData);
 
   return (
+
     <div className="wrapper">
       <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
+      <Routes>
+        <Route path="/" element={<Home employees={employees} owners={owners} pets={pets}/>}/>
+        
+        <Route path="/staff"
+        element={<StaffList employees={employees} />}
+        />
+
+        {/* <Route path="/pets"
+        element={<PetsList pets={pets} /> }/> */}
+
+       <Route path="/pets/"
+        element={<Navigate to="/pets/cats"/>}
+       />
+
+      <Route path="/pets/"
+        element={<Navigate to="/pets/dogs"/>}
+       />
+
+       <Route path="pets/cats" element={<PetsList pets={pets}/>}/>
+       <Route path="pets/dogs" element={<PetsList pets={pets}/>}/>
+
+
+
+
+       
+
+       {/* <Route path="/" */}
+
+
+      {/* <StaffList employees={employees} />
+       
+      <PetsList pets={pets} /> */}
+      </Routes>
       <Footer />
     </div>
+  
   );
 }
 
