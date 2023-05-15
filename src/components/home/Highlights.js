5447e450b58e29466eb5e42c70c85c5c910ffc16
Lakisha J <lakishajohnson@pursuit.org>
2023-05-15 19:01:04 -0400

    Removed .length from props

--- src/components/home/Highlights.js
+++ src/components/home/Highlights.js
@@ -4,15 +4,15 @@ export const Highlights = ({ employees, owners, pets }) => {
   return (
     <section className="highlights">
       <article>
-        <p className="numerical">{employees.length}</p>
+        <p className="numerical">{employees}</p>
         <p>Pawesome staff members!</p>
       </article>
       <article>
-        <p className="numerical">{pets.length}</p>
+        <p className="numerical">{pets}</p>
         <p>Furry children supported!</p>
       </article>
       <article>
-        <p className="numerical">{owners.length}</p>
+        <p className="numerical">{owners}</p>
         <p>Absolutely purrfect clients!</p>
       </article>
     </section>
