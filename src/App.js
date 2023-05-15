814bd59febfe7ccb6f7f3da94aa2066dacdb3e48
Judy <judychue@pursuit.org>
2023-05-15 09:48:23 -0400

    Passed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-// import "./index.css";
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+// import {Link} from "react-router-dom";
 /*
   Components
 */
@@ -23,40 +24,81 @@ function App() {
   const [employees] = useState(employeeData);
   const [owners] = useState(ownerData);
   const [pets] = useState(petData);
-  const cats = pets.filter((pet) => {
-    return pet.kind === "Cat"
-  });
-  const dogs = pets.filter((pet) => {
-    return pet.kind === "Dog"
-  })
+
+  const [cats, dogs] = pets.reduce(
+    (acc, pet) => {
+      const position = pet.kind === "Cat" ? 0 : 1;
+      acc[position].push(pet);
+      return acc;
+    },
+    [[], []]
+  );
 
   return (
     <div className="wrapper">
-          <Router>
-          <Nav />
-            <Routes>
-              <Route path= "/" element = {<Home employees={employees} owners={owners} pets={pets}/>}/>
-              <Route path= "/staff" element = {<StaffList employees={employees} />}/>
-              <Route 
-                  path= "/pets"
-                  element = {<PetsList pets={pets} kind = {'pets/dogs'}/>}
-                  />
-              <Route 
-                  path= "/pets/cats"
-                  element = {<PetsList pets={cats} kind = {'cats'}/>}
-                  />
-              <Route 
-                  path= "/pets/dogs"
-                  element = {<PetsList pets={dogs} kind = {'dogs'}/>}
-                  />
-
-          {/*
-          <PetsList pets={pets} /> */}
-          </Routes>
-          </Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home employees={employees} owners={owners} pets={pets} />}/>
+        <Route path="/staff" element={<StaffList employees={employees} />}/>
+        <Route path="/pets/*" element={<Navigate to="/pets/cats"/>}/>
+        <Route path="/pets/cats" element={<PetsList cats={cats} dogs={dogs} path={'cats'}/>} />
+        <Route path="/pets/dogs" element={<PetsList cats={cats} dogs={dogs} path={'dogs'}/>} />
+      </Routes>
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+
+// import "./index.css";
+// import { useState } from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { Navigate } from 'react-router-dom';
+/*
+  Components
+*/
+// import Nav from "./components/common/Nav";
+// import Footer from "./components/common/Footer";
+// import Home from "./components/home/Home";
+// import StaffList from "./components/staff/StaffList";
+// import PetsList from "./components/pets/PetsList";
+
+/*
+  Data
+  ---------------
+  Note: Normally this data would be pulled from an API. It is not necessary, however, for this application.
+*/
+// import { employeeData } from "./data/employees.js";
+// import { ownerData } from "./data/owners";
+// import { petData } from "./data/pets";
+
+// function App() {
+//   const [employees] = useState(employeeData);
+//   const [owners] = useState(ownerData);
+//   const [pets] = useState(petData);
+//   const cats = pets.filter((pet) => {
+//     return pet.kind === "Cat"
+//   });
+//   const dogs = pets.filter((pet) => {
+//     return pet.kind === "Dog"
+//   })
+
+//   return (
+//     <div className="wrapper">
+//           <Router>
+//           <Nav />
+//             <Routes>
+//               <Route path= "/" element = {<Home employees={employees} owners={owners} pets={pets}/>}/>
+//               <Route path= "/staff" element = {<StaffList employees={employees} />}/>
+//               <Route path="/pets/*" element={<PetsList pets={pets} />}/>
+//               <Route path="/pets" element={<Navigate to="/pets/cats"/>} />
+//           </Routes>
+//           </Router>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -1,23 +1,25 @@
-import {Link} from "react-router-dom";
+//opt 2
+
 import "./Nav.css";
+import { Link } from "react-router-dom";
+import PetsList from "../pets/PetsList";
+import StaffList from "../staff/StaffList";
 
 function Nav() {
   return (
     <header>
       <article>
         <h1>
-          <Link to="/">
-            Northwest <span>Animal Hospital</span>
-          </Link>
+        <Link to="/" element={<StaffList/>}>Northwest <span>Animal Hospital</span></Link>
         </h1>
       </article>
       <aside>
         <ul>
           <li>
-            <Link to="/staff">All Staff</Link>
+            <Link to="/staff" element={<StaffList/>}>All Staff</Link>
           </li>
           <li>
-            <Link to="/pets">All Pets</Link>
+            <Link to="/pets" element={<PetsList/>}>All Pets</Link>
           </li>
         </ul>
       </aside>
@@ -26,3 +28,34 @@ function Nav() {
 }
 
 export default Nav;
+
+
+
+// import {Link} from "react-router-dom";
+// import "./Nav.css";
+
+// function Nav() {
+//   return (
+//     <header>
+//       <article>
+//         <h1>
+//           <Link to="/">
+//             Northwest <span>Animal Hospital</span>
+//           </Link>
+//         </h1>
+//       </article>
+//       <aside>
+//         <ul>
+//           <li>
+//             <Link to="/staff">All Staff</Link>
+//           </li>
+//           <li>
+//             <Link to="/pets">All Pets</Link>
+//           </li>
+//         </ul>
+//       </aside>
+//     </header>
+//   );
+// }
+
+// export default Nav;
