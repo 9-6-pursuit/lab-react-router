73d419e9fcd1f35edf1055d87dd822719284025e
Lakisha J <lakishajohnson@pursuit.org>
2023-05-22 21:34:30 -0400

    Added Highlights

--- src/components/home/Highlights.js
+++ src/components/home/Highlights.js
@@ -4,15 +4,15 @@ export const Highlights = ({ employees, owners, pets }) => {
   return (
     <section className="highlights">
       <article>
-        <p className="numerical">{employees}</p>
+        <p className="numerical">{employees.length}</p>
         <p>Pawesome staff members!</p>
       </article>
       <article>
-        <p className="numerical">{pets}</p>
+        <p className="numerical">{pets.length}</p>
         <p>Furry children supported!</p>
       </article>
       <article>
-        <p className="numerical">{owners}</p>
+        <p className="numerical">{owners.length}</p>
         <p>Absolutely purrfect clients!</p>
       </article>
     </section>
