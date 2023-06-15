22a034fd67115e5d8e28d7034254ee055f6c5c15
RadiantRay22 <rayrussell@pursuit.org>
2023-06-15 08:46:00 -0400

    done

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,6 +1,9 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+
 
 export const PetsList = ({ pets }) => {
   const [cats, dogs] = pets.reduce(
@@ -12,22 +15,30 @@ export const PetsList = ({ pets }) => {
     [[], []]
   );
 
+  const navigate = useNavigate()
+  const kind = useParams()
+  useEffect(()=>{
+    if(!kind.kind){
+      navigate('/pets/cats')
+    }
+  },[])
+
   return (
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
         {/* All cats section */}
-        {cats.map((cat) => (
+
+        {kind.kind === 'cats' ?cats.map((cat) => (
           <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+          )) : null}
 
-        {/* All dogs section */}
-        {dogs.map((dog) => (
+{kind.kind === 'dogs' ? dogs.map((dog) => (
           <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
-      </section>
-    </section>
-  );
-};
+          )) : null}
+          </section>
+        </section>
+      );
 
+       };
 export default PetsList;
